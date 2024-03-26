7b024ba91c92593e6c8df04f5678cb5836fab483
Maciej Duszczak <owndragon@protonmail.com>
2024-03-26 19:39:13 +0100

    Added stations usage calculation and some utility functions

--- main.cpp
+++ main.cpp
@@ -4,25 +4,34 @@
 #include <ctime>
 #include <algorithm>
 #include <iterator>
+#include <random>
 class Config{ /* Configuration */
     private:
         unsigned N; //num of base stations
         unsigned R; //num of resources
         unsigned sim_time; //time of sim
+        float L;
+        float H;
     public:
         Config(){
             this->N = 3;
             this->R = 273;
-            this->sim_time = 1*60*60*1000;
+            this->sim_time = 24*60*60*1000;
+            this->L=0.2;
+            this->H=0.8;
         }
-        Config(unsigned N, unsigned R, unsigned sim_time){
+        Config(unsigned N, unsigned R, unsigned sim_time, float L, float H){
             this->N = N;
             this->R = R;
             this->sim_time = sim_time;
+            this->L=0.2;
+            this->H=0.8;
         }
         unsigned num_of_stations();
         unsigned num_of_resources();
         unsigned get_sim_end_time();
+        float getL();
+        float getH();
 };
 
 unsigned Config::num_of_stations(){
@@ -37,31 +46,40 @@ unsigned Config::get_sim_end_time(){
     return sim_time;
 }
 
+float Config::getL(){
+    return L;
+}
+
+float Config::getH(){
+    return H;
+}
 class Simulation: public Config{
     public:
         bool dbg;
         unsigned time,end_time;
-        unsigned event_counter;
-        float intensity;
-        float factor;
+        unsigned event_counter,users_counter;
+        unsigned hours;
+        double intensity,factor,lambda;
+        float L,H;
         Simulation(){
             this->time = 0;
             this->end_time = get_sim_end_time();
             this->dbg = false;
-            factor = 0.5;
-            this->intensity = factor*1.0;
+            this->factor = 0.5;
+            this->lambda = 5;
+            this->intensity = factor*lambda;
             this->event_counter = 0;
+            this->hours = 60*60*1000;
+            this->L = getL();
+            this->H = getH();   
         }
         bool run();
         void debug();
         void changeIntensity();
-        float getIntensity();
-        unsigned currSimTime();
-        void incrementTime();
-        void incrementTime(unsigned);
+        double getIntensity();
 };
 
-float Simulation::getIntensity(){
+double Simulation::getIntensity(){
     return intensity;
 }
 
@@ -72,37 +90,32 @@ void Simulation::changeIntensity(){
     /*      14 - 18   |     1       */
     /*      18 - 24   |    0.75     */   
     // convert to hours and if stime>=24 [h] reset to 0.
-    if ( (time >= 0)&&(time <= 8)&&(factor!=0.5) ){ // 0 - 8 [h] 
+    if ( (time >= 0)&&(time <= 8*hours)&&(factor!=0.5) ){ // 0 - 8 [h] 
         factor = 0.5;
         float old_intensity = intensity;
-        intensity = factor*(1);
+        intensity = factor*lambda;
         std::cout << "Intensity changed from:"<<old_intensity<<" to:"<<intensity<<std::endl;
     }
-    else if ((time >= 8)&&(time <= 14)&&(factor!=0.75)){ // 8 - 14 [h]
+    else if ((time >= 8*hours)&&(time <= 14*hours)&&(factor!=0.75)){ // 8 - 14 [h]
         factor = 0.75;
         float old_intensity = intensity;
-        intensity = factor*(1);
+        intensity = factor*lambda;
         std::cout << "Intensity changed from:"<<old_intensity<<" to:"<<intensity<<std::endl;
     }
-    else if ((time >= 14)&&(time <= 18)&&(factor!=1.0)){ // 14-18 [h]
+    else if ((time >= 14*hours)&&(time <= 18*hours)&&(factor!=1.0)){ // 14-18 [h]
         factor = 1;
         float old_intensity = intensity;
-        intensity = factor*(1);
+        intensity = factor*lambda;
         std::cout << "Intensity changed from:"<<old_intensity<<" to:"<<intensity<<std::endl;
     }
-    else if ((time >= 18)&&(time <= 24)&&(factor!=0.75)){ // 18-24 [h]
+    else if ((time >= 18*hours)&&(time <= 24*hours)&&(factor!=0.75)){ // 18-24 [h]
         factor = 0.75;
         float old_intensity = intensity;
-        intensity = factor*(1);
+        intensity = factor*lambda;
         std::cout << "Intensity changed from:"<<old_intensity<<" to:"<<intensity<<std::endl;
     }
-    else //Should never get here
+    else 
         return;
-        //abort();
-}
-
-unsigned Simulation::currSimTime(){
-    return time;
 }
 
 void Simulation::debug(){
@@ -115,28 +128,15 @@ bool Simulation::run(){
     return false;
 }
 
-void Simulation::incrementTime(){
-    time+=10;
-}
-
-void Simulation::incrementTime(unsigned increment){
-    //if((time+increment)<=get_sim_end_time())
-    time+=increment;
-    if(dbg){
-        std::cout<<"Incremented sim time by "<<increment<<"."
-        <<std::endl<<"Current sim time: "<<time<<"."<<std::endl;
-    }
-}
-
 class User {
     public:
         unsigned service_start;
         unsigned service_time;
         unsigned service_end;
-        User(unsigned stime){
-            this->service_start = stime;
-            this->service_time = 1000+rand()%30000;
-            this->service_end = stime+service_time;
+        User(unsigned sim_time){
+            this->service_start = sim_time;
+            this->service_time = 1000+rand()%30000; // 1-30[s]
+            this->service_end = sim_time+service_time;
             //print();
         }
         void print();
@@ -172,6 +172,19 @@ bool compareEvents(Event e1, Event e2){
 bool compareUsers(User u1, User u2){
     return (u1.service_time > u2.service_time);
 }
+
+class exponentialGenerator{
+    public:
+        std::random_device rd;
+        exponentialGenerator(){}
+        double get(double);
+};
+
+double exponentialGenerator::get(double intensity){
+    std::exponential_distribution<double> exp_dist(intensity);
+    std::mt19937 gen(rd());
+    return 1000*exp_dist(gen);
+}
 class BaseStation: public Config{
     public:
         unsigned id,resources,power;
@@ -179,21 +192,43 @@ class BaseStation: public Config{
         unsigned next_user_time;
         bool hibernate;
         std::vector<User> users_list;
+        exponentialGenerator* gen = nullptr;
         BaseStation(){
             this->resources = num_of_resources();
             this->power = 0;
             this->users_rejected = 0;
             this->hibernate = false;    
             users_list.clear();
+            gen = new exponentialGenerator();
         }   
         unsigned num_of_users();
-        bool executeEvent(Event,std::vector<Event>*,Simulation*);
+        double currentUsage();
+        bool executeEvent(Event,std::vector<Event>*,Simulation*,bool);
         Event scheduleEvent(Simulation*);
         void getNextUserArrivalTime(Simulation*);
         void printUsersList();
+        void activate();
+        void deactivate();
+        bool isHibernated();
         bool isFull();
         bool isEmpty();
 };
+
+void BaseStation::activate(){
+    hibernate = false;
+}
+
+void BaseStation::deactivate(){
+    hibernate = true;
+}
+
+bool BaseStation::isHibernated(){
+    return hibernate;
+}
+
+double BaseStation::currentUsage(){
+    return static_cast<double>(num_of_users())/resources;
+}
 
 void BaseStation::printUsersList(){
     for (unsigned i=0;i<users_list.size();i++){
@@ -201,22 +236,23 @@ void BaseStation::printUsersList(){
     }
 }
 
-bool BaseStation::executeEvent(Event event, std::vector<Event>* event_list, Simulation* sim){   
+bool BaseStation::executeEvent(Event event, std::vector<Event>* event_list, Simulation* sim, bool handover){   
     //std::cout<<"Event type:"<<event.type<<" time:"<<event.time<<std::endl; 
     if (event.type == 'A'){
-        // Schedule user departure event if user is added
-        if (!isFull()){
-            User usr(sim->time);
-            Event event(usr.service_end,id,'D');
-            //std::cout<<"Added user to station:"<<id<<" User end time:"<<usr.service_end<<std::endl;
-            users_list.push_back(usr);
-            event_list->push_back(event);
-            /* Now schedule next user arriving at that station */
+        // Schedule next user arriving at that station
+        if (!handover){ 
+            sim->users_counter++;
             getNextUserArrivalTime(sim);
             Event eve(sim->time + next_user_time,id,'A');
             event_list->push_back(eve);
-            sim->event_counter+=2;
-            // -> schedule another arrival at station <-
+        }
+        // Schedule user departure event if user is added
+        if (!isFull()){
+            User usr(sim->time);
+            users_list.push_back(usr);
+            Event event(usr.service_end,id,'D');
+            event_list->push_back(event);
+            //std::cout<<"Added user to station:"<<id<<" User end time:"<<usr.service_end<<std::endl;
             return true;
         }
         else //Handover to another station
@@ -235,14 +271,18 @@ bool BaseStation::executeEvent(Event event, std::vector<Event>* event_list, Simu
 
 Event BaseStation::scheduleEvent(Simulation* sim){
     getNextUserArrivalTime(sim);
-    //std::cout<<"STIME:"<<sim->time<<std::endl;
     Event event(next_user_time + sim->time, id, 'A');
     return event;
 }
 
 void BaseStation::getNextUserArrivalTime(Simulation* sim){
-    //sim->changeIntensity(); 
-    next_user_time = 1000+rand()%1000;
+    //std::random_device rd;
+    //std::mt19937 gen(rd());
+    //std::exponential_distribution<double> exp_dist(sim->intensity);
+    // Generate a random value
+    sim->changeIntensity(); 
+    next_user_time = gen->get(sim->intensity); //1000*exp_dist(gen);
+
 }
 
 bool BaseStation::isEmpty(){
@@ -265,11 +305,11 @@ class Network: public Config{
     public:
         std::vector<BaseStation> stations;
         std::vector<Event> events_list;
-        unsigned total_users;
+        double total_usage;
         unsigned next_event_time;
         unsigned next_user;
         Network(){
-            total_users = 0;
+            total_usage = 0;
             stations.clear();
             events_list.clear();
             for(unsigned i=0;i<num_of_stations();i++){
@@ -282,6 +322,8 @@ class Network: public Config{
         unsigned get_id_station_least_users();
         bool all_stations_empty();
         bool all_stations_full();
+        void calcStationsUsage(unsigned);
+        unsigned calcTotalUsersRejected();
         /* Utility */
         void printEventList();
         void printUsersAtStations();
@@ -289,12 +331,28 @@ class Network: public Config{
         /**********/
 };
 
+unsigned Network::calcTotalUsersRejected(){
+    unsigned sum = 0;
+    for (auto x : stations){
+        sum+=x.users_rejected;
+    }
+    return sum;
+}
+
+void Network::calcStationsUsage(unsigned duration){
+    double usage = 0.00;
+    unsigned N = num_of_resources();
+    for (auto x : stations){
+        usage += x.currentUsage();
+    }
+    total_usage+=duration*(usage/num_of_stations());
+}
+
 void Network::printUsersAtStations(){
     std::cout<<"------------------"<<std::endl;
     for(unsigned i=0;i<num_of_stations();i++){
         std::cout<<"Station"<<i<<" has "<<stations[i].users_list.size()<<" users."<<std::endl;
     }
-    std::cout<<"------------------"<<std::endl;
 }
 
 unsigned Network::get_id_station_least_users(){
@@ -318,49 +376,47 @@ void Network::printEventList(){
 
 void Network::methodABC(Simulation* sim){
     /* Phase A: Event Scheduling  */
-    if ( events_list.empty() ){
+    if (events_list.empty()){ // Initial state of network
         for (unsigned i=0;i<num_of_stations();i++){
-            // Schedule arrival event
+            // Schedule first arrival events at all stations
             events_list.push_back(stations[i].scheduleEvent(sim));
-            //std::cout<<"E"<<i<<": "<<events_list[i].time<<std::endl;
-            sim->event_counter++;
         }
     }
     //std::cout<<"---"<<std::endl;
     // Sort event list
     sort(events_list.begin(), events_list.end(), compareEvents);
     //printEventList();
-    // Pop next event from calendar 
-    Event next_event = events_list.back();
-    // Remove event from calendar
-    events_list.pop_back();
+    Event next_event = events_list.back(); // Pop next event from calendar 
+    sim->event_counter++;
+    events_list.pop_back(); // Remove event from calendar
     // Increment sim time to nearest event
-    if (next_event.time > get_sim_end_time()){
-        sim->time = get_sim_end_time();
+    if (next_event.time > sim->end_time){ //If next event is after sim end time
+        calcStationsUsage(sim->end_time - sim->time);
+        sim->time = sim->end_time;
         return;
     }
+    calcStationsUsage(next_event.time - sim->time);
     sim->time = next_event.time;
     // Execute nearest event
-    if(!stations[next_event.sid].executeEvent(next_event, &events_list, sim)){
-        // If station is FULL, handover event to non full station
-        // OR drop it.
+    if(!stations[next_event.sid].executeEvent(next_event, &events_list, sim, false)){
+        // If station is FULL, handover event to non full station OR drop it.
         if ( all_stations_full() ){
             //if all stations are full, drop
             //std::cout<<"User dropped for station:"<<next_event.sid<<std::endl;
+            stations[next_event.sid].users_rejected++;
         }
         else{ //Try adding user to station with smallest amount of users
             next_event.sid = get_id_station_least_users();
-            if (!stations[next_event.sid].executeEvent(next_event, &events_list, sim)){
+            if (!stations[next_event.sid].executeEvent(next_event, &events_list, sim, true)){
                 abort(); //This acually should never happen
             }
         }
     } 
-    else
-        total_users++;
     // Sort events again
-    sort(events_list.begin(), events_list.end(), compareEvents);
+    //sort(events_list.begin(), events_list.end(), compareEvents);
     //printEventList();
-    std::cout<<"DONE: Phase A -> SIM TIME = "<<sim->time<<" [ms]"<<std::endl;
+
+    //std::cout<<"DONE: Phase A -> SIM TIME = "<<sim->time<<" [ms]"<<std::endl;
     //std::cout<<"TOTAL USERS AT STATIONS: "<<total_users<<std::endl;
     //std::cout<<"TOTAL EVENTS: "<<sim->event_counter<<std::endl;
     /*****************************/
@@ -389,35 +445,44 @@ void Network::config(){
     std::cout<<"--- NETWORK CONFIG ---"<<std::endl;
     std::cout<<"N:"<<num_of_stations()<<std::endl;
     std::cout<<"R:"<<num_of_resources()<<std::endl;
-    std::cout<<"SIM_END_TIME:"<<get_sim_end_time()<<std::endl;
+    std::cout<<"SIM_END_TIME:"<<get_sim_end_time()<<" [ms]"<<std::endl;
     for(unsigned i=0;i<num_of_stations();i++)
         std::cout<<"STATION:"<<stations[i].id<<
         " RESOURCES:"<<stations[i].resources<<std::endl;
     std::cout<<"----------------------"<<std::endl;
 }
 
-void simulate(Simulation sim, Network net, unsigned iters=1000000){
+void simulate(Simulation sim, Network net, unsigned iters=10000){
     net.config();
     unsigned i = 0;
-    while ( sim.run() /* && (i<=iters) */ ){
+    while ( sim.run() /* && (i<=iters)*/ ){
         //std::cout<<"----------------------"<<std::endl;
         net.methodABC(&sim);
         //net.printUsersAtStations();
         //std::cout<<"SID:"<<net.get_id_station_least_users()<<std::endl;
+        //net.printUsersAtStations();
         i++;
-    }
-    std::cout<<"TOTAL EVENTS:"<<sim.event_counter;
+    }   
+    net.printUsersAtStations();
+    std::cout<<"TOTAL EVENTS: "<<sim.event_counter<<std::endl;
+    std::cout<<"AVERAGE USAGE: "<<100*(net.total_usage/sim.get_sim_end_time())<<"%"<<std::endl;
+    std::cout<<"TOTAL USERS CREATED: "<<sim.users_counter<<std::endl;
+    std::cout<<"USERS REJECTED: "<<100*(static_cast<double>(net.calcTotalUsersRejected())/sim.users_counter)<<"%";
 }
 
-int main(){
+int main(void){
     srand(time(NULL)); 
+    std::clock_t c_start = std::clock();
 
+    //freopen("simlog.txt","w",stderr);
     Config cfg; //Create config for simulation
     Simulation sim; //Init simulation with given config
     sim.debug();
     Network net; //Create the network
-
     simulate(sim, net);
-    
+
+    std::clock_t c_end = std::clock();
+    long double time_elapsed_ms = 1000.0 * (c_end-c_start) / CLOCKS_PER_SEC;
+    std::cout <<std::endl<< "CPU time used: " << time_elapsed_ms << " ms\n";
     return 0;
 }
